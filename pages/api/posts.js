3c5c95338ce8d1f9910c1c50dc73f6dded8f5372
Anthony Corcutt <acorcutt@gmail.com>
2021-06-23 20:14:04 +0000

    try get server

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -2,8 +2,9 @@ import next from 'next';
 
 export default async function handler(req, res) {
   const app = next({ dev: process.env.NODE_ENV !== 'production' });
-  await app.prepare();
-  const keys = await app.server.incrementalCache.cache.keys() 
-  console.dir(app.server)
-  res.status(200).json(keys)
+  //const p = await app.prepare();
+  const server = await app.getServer();
+  const keys = server.incrementalCache.cache.keys() 
+  console.dir(server.incrementalCache)
+  res.status(200).json({keys, length: server.incrementalCache.cache.length })
 }
